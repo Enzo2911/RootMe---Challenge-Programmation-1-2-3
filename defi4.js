4f26f59a45ad73ff5a1ccce90ea75e93f67fddb3
Enzo2911 <38391212+Enzo2911@users.noreply.github.com>
2020-04-08 16:04:09 +0200

    Add files via upload

---
+++ defi4.js
@@ -0,0 +1,26 @@
+const irc = require('irc');
+const { unzip } = require('zlib');
+
+var client = new irc.Client('irc.root-me.org', 'Enzo2911', {
+    channels: ['#root-me_challenge'],
+	debug: true
+});
+client.addListener('registered', function (message) {
+ 	client.say("Candy", "!ep4");
+  });
+	
+client.addListener('pm', function (from, message) {
+	console.log("connected")
+  const regex = /[a-zA-Z=0-9]/gm;
+  const msg = message.match(regex);
+  const deux = msg.join("")
+  const buffer = Buffer.from(deux, 'base64');
+  
+  unzip(buffer, (err, buffer) => {
+  if (err) {
+    console.error('Erreur décompression: ', err);
+  } else {
+    client.say("Candy", "!ep4 -rep " + buffer.toString())
+  }
+  });
+});
